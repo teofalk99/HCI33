bc13ef9391864266697cb59aa50932c0816c593d
BenWonderlin <ben.wonderlin@yale.edu>
2023-04-09 15:31:25 -0400

    set up website template from class

---
+++ main.js
@@ -0,0 +1,44 @@
+var host = "cpsc484-04.yale.internal:8888";
+
+$(document).ready(function () {
+    frames.start();
+    twod.start();
+});
+
+var frames = {
+    socket: null,
+
+    start: function () {
+        var url = "ws://" + host + "/frames";
+        frames.socket = new WebSocket(url);
+        frames.socket.onmessage = function (event) {
+            frames.show(JSON.parse(event.data));
+        }
+    },
+
+    show: function (frame) {
+        console.log(frame);
+    }
+};
+
+var twod = {
+    socket: null,
+
+    // create a connection to the camera feed
+    start: function () {
+        var url = "ws://" + host + "/twod";
+        twod.socket = new WebSocket(url);
+
+        // whenever a new frame is received...
+        twod.socket.onmessage = function (event) {
+
+            // parse and show the raw data
+            twod.show(JSON.parse(event.data));
+        }
+    },
+
+    // show the image by adjusting the source attribute of the HTML img object previously created
+    show: function (twod) {
+        $('img.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
+    },
+};
