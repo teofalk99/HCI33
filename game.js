2b28d4718fa2b2ea34a72ffc9f7b6541ffd32326
MatthewQiu-5 <matthewqiu42@gmail.com>
2023-04-25 12:53:46 -0400

    changed relative import of game assets

--- game.js
+++ game.js
@@ -50,14 +50,14 @@ TV_ASSET_URL = '/project/HCI33'
 
 function preload ()
 {
-    this.load.setBaseURL(TV_ASSET_URL)
+    // this.load.setBaseURL(TV_ASSET_URL)
     // These assets are from the Phaser3 library
-    this.load.image('paddle1', 'assets/paddle1.png');
-    this.load.image('ball1', 'assets/ball1.png');
-    this.load.image('paddle2', 'assets/paddle2.png');
-    this.load.image('ball2', 'assets/ball2.png');
-    this.load.image('brick', 'assets/green1.png');
-    this.load.image('background', 'assets/pixelback1.jpg');
+    this.load.image('paddle1', './assets/paddle1.png');
+    this.load.image('ball1', './assets/ball1.png');
+    this.load.image('paddle2', './assets/paddle2.png');
+    this.load.image('ball2', './assets/ball2.png');
+    this.load.image('brick', './assets/green1.png');
+    this.load.image('background', './assets/pixelback1.jpg');
 }
 
 // Game Objects
